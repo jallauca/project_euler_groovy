9d3b2d5973e0c99672144ab9f1d2415c0462a2cb
Jaime Allauca <jallauca@yahoo.com>
2014-07-13 12:39:32 -0400

    Solve problem 4

---
+++ problem_4.groovy
@@ -0,0 +1,39 @@
+// Largest palindrome product (Problem 4) (https://projecteuler.net/problem=4)
+
+// A palindromic number reads the same both ways. The largest palindrome made
+// from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+// Find the largest palindrome made from the product of two 3-digit numbers.
+
+def n_digit_numbers(int n) {
+    (10..<10.power(n))
+}
+
+ def is_palindrome(int n) {
+    n.toString() == n.toString().reverse()
+}
+
+def find_palindromes_of_products(List<Integer> numbers) {
+    ( 0..numbers.size().div(2)).inject([ ] ) { palindromes, x ->
+        palindrome = (0..x).findResult { y ->
+            product = numbers[x] * numbers[y]
+            is_palindrome(product) ? product : null
+        }
+        if ( palindrome ) palindromes << palindrome
+        palindromes
+    }
+}
+
+assert is_palindrome(1)
+assert !is_palindrome(10)
+assert is_palindrome(101)
+assert !is_palindrome(5010)
+assert is_palindrome(50105)
+
+def two_digit_numbers = n_digit_numbers(2).reverse()
+assert find_palindromes_of_products( two_digit_numbers ).max() == 9009
+
+def three_digit_numbers = n_digit_numbers(3).reverse()
+assert find_palindromes_of_products( three_digit_numbers ).max() == 906609
+
+println "tests pass"
