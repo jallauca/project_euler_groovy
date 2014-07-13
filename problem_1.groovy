0766dbbac5c0f19f366992734aaf040b38d39286
Jaime Allauca <jallauca@yahoo.com>
2014-07-13 12:39:21 -0400

    Add problem description

--- problem_1.groovy
+++ problem_1.groovy
@@ -1,3 +1,5 @@
+// Multiples of 3 and 5 (Problem 1) (https://projecteuler.net/problem=1)
+
 limit = 1000
 divBy3And5 = ( 1..<limit ).findAll { it % 3 == 0 || it % 5 == 0 }
 assert divBy3And5.sum() == 233168
--- problem_3.groovy
+++ problem_3.groovy
@@ -1,3 +1,22 @@
+// Largest prime factor (Problem 3) (https://projecteuler.net/problem=3)
+
+// The prime factors of 13195 are 5, 7, 13 and 29.
+// What is the largest prime factor of the number 600851475143 ?
+
+// (http://en.wikipedia.org/wiki/Prime_number)
+// The most basic method of checking the primality of a given integer n is called
+// trial division. This routine consists of dividing n by each integer m that is
+// greater than 1 and less than or equal to the square root of n. If the result
+// of any of these divisions is an integer, then n is not a prime, otherwise it
+// is a prime. Indeed, if n=a b is composite (with a and b ≠ 1) then one of the
+// factors a or b is necessarily at most \sqrt{n}. For example, for  n = 37 , the
+// trial divisions are by m = 2, 3, 4, 5, and 6. None of these numbers divides
+// 37, so 37 is prime. This routine can be implemented more efficiently if a
+// complete list of primes up to \sqrt{n} is known—then trial divisions need to
+// be checked only for those m that are prime. For example, to check the primality
+// of 37, only three divisions are necessary (m = 2, 3, and 5), given that 4 and
+// 6 are composite.
+
 class PrimeGenerator implements Iterator<Long> {
     def primes = [2L,3L]
     def position = 0;
