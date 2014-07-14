a252f445f1f68aa153c3cd5541a5d7343325a4da
Jaime Allauca <jallauca@yahoo.com>
2014-07-13 22:05:54 -0400

    Refactor to improve readability

--- problem_4.groovy
+++ problem_4.groovy
@@ -6,20 +6,20 @@
 // Find the largest palindrome made from the product of two 3-digit numbers.
 
 // Solution Tips:
-// Reverse the list of numbers in decreasing order so that finding the
-// first palindrome yields the highest one.
+// Iterate x and y, where x in numbers and y in numbers in descending order,
+// so that the first x*y that is palindrome means returning the highest
 
 def n_digit_numbers(int n) {
-    (10..<10.power(n))
+    (10.power(n-1)..<10.power(n))
 }
 
  def is_palindrome(int n) {
     n.toString() == n.toString().reverse()
 }
 
-def find_palindromes_of_products(List<Integer> numbers) {
-    def result = ( 0..<numbers.size() ).collect { int x ->
-        palindrome = (x..0).findResult { int y ->
+def find_each_palindrome_of_products(List<Integer> numbers) {
+    ( numbers.size()-1..<0 ).collect { int x ->
+        palindrome = (x-1..0).findResult { int y ->
             product = numbers[x] * numbers[y]
             is_palindrome(product) ? product : null
         }
@@ -36,7 +36,7 @@ assert is_palindrome(50105)
 def two_digit_numbers = n_digit_numbers(2)
 def three_digit_numbers = n_digit_numbers(3)
 
-assert find_palindromes_of_products( three_digit_numbers ).max() == 906609
-assert find_palindromes_of_products( two_digit_numbers ).max() == 9009
+assert find_each_palindrome_of_products( three_digit_numbers ).max() == 906609
+assert find_each_palindrome_of_products( two_digit_numbers ).max() == 9009
 
 println "tests pass"
