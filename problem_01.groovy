453c9557dcb322a6ad3346c8983172db7e5042b7
Jaime Allauca <jallauca@yahoo.com>
2014-07-15 22:14:50 -0400

    Refactor to improve readability

--- problem_01.groovy
+++ problem_01.groovy
@@ -1,6 +1,9 @@
 // Multiples of 3 and 5 (Problem 1) (https://projecteuler.net/problem=1)
 
 limit = 1000
-divBy3And5 = ( 1..<limit ).findAll { it % 3 == 0 || it % 5 == 0 }
-assert divBy3And5.sum() == 233168
+answer = ( 1..<limit ).findAll { it % 3 == 0 || it % 5 == 0 }.sum()
+assert answer == 233168
+
 println "tests pass"
+
+println "answer=$answer"
