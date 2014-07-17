8a575258586cf303b5eacae7af6c5426a7920ad6
Jaime Allauca <jallauca@yahoo.com>
2014-07-16 21:40:26 -0400

    Replace eachConsecutive with step with grid_consecutive_vertical

--- problem_11.groovy
+++ problem_11.groovy
@@ -1,17 +1,15 @@
 evaluate(new File("IterableMonkeyPatch.groovy"))
 
-Iterable.metaClass.eachConsecutive = { int n, step = 1 ->
-    max_index = delegate.size()-(step * (n - 1))
-    (0..<max_index).collect {
-        range = (it..it+n*(step-1)).step(step)
-        delegate[range]
-    }
-}
-
 grid_count = 4
 consecutive = 3
 
 def number_grid(n) { (0..<n.power(2)) }
+
+def grid_consecutive_vertical = { int n, step = 3 ->
+    number_grid(n)
+    .collect { x -> [x, x + n, x + n*2] }
+    .findAll { x, y, z -> z < n.power(2) }
+}
 
 def grid_consecutive_incline_right = { int n, step = 3 ->
     number_grid(n)
@@ -27,13 +25,11 @@ def grid_consecutive_incline_left = { int n, step = 3 ->
     .findAll { _, y, z -> z < n.power(2) }
 }
 
-assert number_grid(3).eachConsecutive(3, 3) == [
-    [0, 3, 6], [1, 4, 7], [2, 5, 8]]
-
+assert grid_consecutive_vertical(3) == [[0, 3, 6], [1, 4, 7], [2, 5, 8]]
 assert grid_consecutive_incline_right(3) == [[0, 4, 8]]
 assert grid_consecutive_incline_left(3) == [[2, 4, 6]]
 
-assert number_grid(4).eachConsecutive(3, 4) == [
+assert grid_consecutive_vertical(4) == [
     [0, 4, 8],  [1, 5, 9],  [2, 6, 10],  [3, 7, 11],
     [4, 8, 12], [5, 9, 13], [6, 10, 14], [7, 11, 15]]
 
@@ -45,7 +41,7 @@ assert grid_consecutive_incline_left(4) == [
     [2, 5, 8], [3, 6, 9],
     [6, 9, 12], [7, 10, 13]]
 
-assert number_grid(5).eachConsecutive(3, 5) == [
+assert grid_consecutive_vertical(5) == [
      [0, 5, 10],   [1, 6, 11],   [2, 7, 12],   [3, 8, 13],   [4, 9, 14],
      [5, 10, 15],  [6, 11, 16],  [7, 12, 17],  [8, 13, 18],  [9, 14, 19],
      [10, 15, 20], [11, 16, 21], [12, 17, 22], [13, 18, 23], [14, 19, 24]]
