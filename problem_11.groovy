a86a2a9b696143a5217c2ee063e7895b1249790a
Jaime Allauca <jallauca@yahoo.com>
2014-07-19 08:39:23 -0400

    Fix tests after refactoring

--- problem_11.groovy
+++ problem_11.groovy
@@ -26,7 +26,7 @@ def numbers_test_string = """
 def number_grid(n) { (0..<n.power(2)) }
 
 def consecutive_horizontal = { int n, int cons = 3, int grid_size ->
-  ( n < grid_size.power(2) && n.div(grid_size) as int == (n+cons-1).div(grid_size) as int ) ?
+  ( n < grid_size.power(2) && (n).div(grid_size) as int == (n+cons-1).div(grid_size) as int ) ?
   ( n..n+cons-1).collect() : []
 }
 
@@ -65,15 +65,13 @@ def max_all_directions = number_grid(grid_size)
            consecutive_vertical(n, cons, grid_size),
            consecutive_incline_upper_left(n, cons, grid_size),
            consecutive_incline_upper_right(n, cons, grid_size)] }
-        .flatten()
-        .collate(4)
+        .flatten().collate(4)
         .collect { it.findAll { it < grid_size.power(2) } }
         .collect { idx -> product_of_each( numbers[idx] ) }
         .max()
 
 assert max_all_directions == 70600674
 println "answer=$max_all_directions"
-
 
 
 
@@ -87,23 +85,29 @@ assert (0..9).collect { consecutive_incline_upper_right(it, 3, 3) } ==
 assert (0..9).collect { consecutive_incline_upper_left(it, 3, 3) } ==
   [[0, 4, 8], [],[],[],[],[],[],[],[],[]]
 
-// assert grid_consecutive_horizontal(4) == [
-//   [0, 1, 2], [1, 2, 3],
-//   [4, 5, 6], [5, 6, 7],
-//   [8, 9, 10], [9, 10, 11],
-//   [12, 13, 14], [13, 14, 15]]
+assert (0..16).collect { consecutive_horizontal(it, 3, 4) } == [
+  [0, 1, 2], [1, 2, 3], [],[],
+  [4, 5, 6], [5, 6, 7], [],[],
+  [8, 9, 10], [9, 10, 11], [],[],
+  [12, 13, 14], [13, 14, 15], [],[],[] ]
 
-// assert grid_consecutive_vertical(4) == [
-//   [0, 4, 8],  [1, 5, 9],  [2, 6, 10],  [3, 7, 11],
-//   [4, 8, 12], [5, 9, 13], [6, 10, 14], [7, 11, 15]]
+assert (0..16).collect { consecutive_vertical(it, 3, 4) } == [
+  [0, 4, 8],  [1, 5, 9],  [2, 6, 10],  [3, 7, 11],
+  [4, 8, 12], [5, 9, 13], [6, 10, 14], [7, 11, 15],
+  [],[],[],[],
+  [],[],[],[],[]]
 
-// assert grid_consecutive_incline_upper_left(4) == [
-//   [0, 5, 10], [1, 6, 11],
-//   [4, 9, 14], [5, 10, 15]]
+assert (0..16).collect { consecutive_incline_upper_left(it, 3, 4) } == [
+  [0, 5, 10], [1, 6, 11],[],[],
+  [4, 9, 14], [5, 10, 15],[],[],
+  [],[],[],[],
+  [],[],[],[],[]]
 
-// assert grid_consecutive_incline_upper_right(4) == [
-//   [2, 5, 8], [3, 6, 9],
-//   [6, 9, 12], [7, 10, 13]]
+assert (0..16).collect { consecutive_incline_upper_right(it, 3, 4) } == [
+  [],[],[2, 5, 8], [3, 6, 9],
+  [],[],[6, 9, 12], [7, 10, 13],
+  [],[],[],[],
+  [],[],[],[],[]]
 
 // assert grid_consecutive_horizontal(5) == [
 //   [0, 1, 2], [1, 2, 3], [2, 3, 4],
