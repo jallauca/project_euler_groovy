046f055edbb95d7909b9bda76ad67ba5e224b524
Jaime Allauca <jallauca@yahoo.com>
2014-07-19 04:06:56 -0400

    Refactor to improve readability

--- problem_01.groovy
+++ problem_01.groovy
@@ -1,7 +1,10 @@
 // Multiples of 3 and 5 (Problem 1) (https://projecteuler.net/problem=1)
 
-limit = 1000
-answer = ( 1..<limit ).findAll { it % 3 == 0 || it % 5 == 0 }.sum()
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+def answer = ( 1..<1000 ).findAll { it % 3 == 0 || it % 5 == 0 }.sum()
 assert answer == 233168
 
 println "tests pass"
