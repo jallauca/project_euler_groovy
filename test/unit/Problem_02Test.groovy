07d48b6e467251efe7efbcdce3cbe6874b733cde
Jaime Allauca <jallauca@yahoo.com>
2014-08-01 22:42:42 -0400

    Move unit testing into separate files

---
+++ test/unit/Problem_02Test.groovy
@@ -0,0 +1,14 @@
+package project.euler.problems
+
+import org.junit.*
+
+class Problem_02Test {
+  @Test
+  void should_include_number_in_summation() {
+    assert new Problem_02().fibonacci(-4) == []
+    assert new Problem_02().fibonacci(0) == []
+    assert new Problem_02().fibonacci(1) == [1]
+    assert new Problem_02().fibonacci(2) == [1,2]
+    assert new Problem_02().fibonacci(4000000).findAll { it % 2  == 0 }.sum() == 4613732
+  }
+}
