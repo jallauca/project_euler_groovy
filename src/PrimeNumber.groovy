66162fd9fcb8ee4e769bc002cb06b195e385cd68
Jaime Allauca <jallauca@yahoo.com>
2014-08-04 16:24:24 -0400

    Fix indent size

--- src/PrimeNumber.groovy
+++ src/PrimeNumber.groovy
@@ -1,30 +1,30 @@
 package project.euler.problems
 
 class PrimeNumber {
-    def static primes_up_to(long max) {
-        new PrimeGenerator().takeWhile { it <= max }.collect()
+  def static primes_up_to(long max) {
+    new PrimeGenerator().takeWhile { it <= max }.collect()
+  }
+
+  def static n_primes(int n) {
+    new PrimeGenerator().take(n).collect()
+  }
+
+  def static List<Long> find_prime_factors(long n) {
+    def primes = primes_up_to( Math.sqrt(n) as long )
+    primes.findAll { n % it == 0 }
+  }
+
+  def static prime_factors(long n) {
+    def primes = PrimeNumber.primes_up_to(n)
+
+    def prime_factors_recrsv
+    prime_factors_recrsv = { long dividend ->
+        if ( dividend <= 1 ) return []
+
+        def prime = primes.find { prime -> dividend % prime == 0 }
+        [prime] + prime_factors_recrsv( dividend.div(prime) as long )
     }
 
-    def static n_primes(int n) {
-        new PrimeGenerator().take(n).collect()
-    }
-
-    def static List<Long> find_prime_factors(long n) {
-        def primes = primes_up_to( Math.sqrt(n) as long )
-        primes.findAll { n % it == 0 }
-    }
-
-    def static prime_factors(long n) {
-        def primes = PrimeNumber.primes_up_to(n)
-
-        def prime_factors_recrsv
-        prime_factors_recrsv = { long dividend ->
-            if ( dividend <= 1 ) return []
-
-            def prime = primes.find { prime -> dividend % prime == 0 }
-            [prime] + prime_factors_recrsv( dividend.div(prime) as long )
-        }
-
-        prime_factors_recrsv(n)
-    }
+    prime_factors_recrsv(n)
+  }
 }
